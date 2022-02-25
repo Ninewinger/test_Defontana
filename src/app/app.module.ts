dcbad919fdb6696073153357753083cfd183d88d
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-02-25 18:57:13 -0300

    subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { EsquemaComponent } from './components/esquema/esquema.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EsquemaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/esquema/esquema.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-esquema",
+    templateUrl: "./esquema.component.html",
+    styleUrls: ["./esquema.component.css"]
+})
+
+export class EsquemaComponent {
+    constructor() { }
+}
